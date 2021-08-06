a7366a8da28b425c2fd90232af7996465366f333
Tommy Chung <tommychung@TC-MacBook-Pro.local>
2021-08-06 16:14:30 -0400

    Start

---
+++ server.js
@@ -0,0 +1,46 @@
+//================Depenedencies================//
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const session = require('express-session')
+const app = express()
+const db = mongoose.connection
+require('dotenv').config()
+
+//================PORT================//
+const PORT = process.env.PORT || 3003;
+const MONGODB_URI = process.env.MONGODB_URI
+
+//================Middleware================//
+
+app.use(express.json())
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
+mongoose.connect(MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    },
+    () => {
+        console.log("The connection with mongod is established")
+})
+
+app.use(cors())
+
+//Route to Heroku
+app.get('/', (req, res) => {
+    res.redirect('/recipes')
+
+
+//================Listener================//
+app.listen(PORT, () => {
+  console.log('🍒🍋Listening on port🥝🍉', PORT)
+})
